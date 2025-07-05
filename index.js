d6d043594ff20ba75437323fb17b663f8301074a
Sanya Safi <sanyasafi94@gmail.com>
2025-07-05 12:04:32 -0500

    added questions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+var quizContain = document.getElementById("quiz");
+
+var questionArr = [
+  {
+    question: "What is another name for a cat?",
+    answer: "Feline",
+    options: ["Monkey", "Cow", "Feline", "Fish"],
+  },
+  {
+    question: "What is the most expressive part of a cat?",
+    answer: "Tail",
+    options: ["Eyes", "Feet", "Mouth", "Tail"],
+  },
+  {
+    question: "What type food do cats eat?",
+    answer: "Meat",
+    options: ["Sun", "Meat", "Veggies", "Fruit"],
+  },
+  {
+    question: "Which type of cat is more physically resiliant than others?",
+    answer: "Black",
+    options: ["Black", "Orange", "Veggies", "Fruit"],
+  },
+  {
+    question: "What is one defining feature of a cat?",
+    answer: "Whiskers",
+    options: ["Ears", "Tail", "Paws", "Whiskers"],
+  },
+];
